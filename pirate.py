f15a48758f0f3ce80da679de219429aded0057e4
davy <davy@windbornesystems.com>
2023-09-03 12:15:20 -0700

    Allow optionally queing after search.

--- pirate.py
+++ pirate.py
@@ -31,25 +31,30 @@ def pick_torrent(query):
         return
 
     for n, t in enumerate(torrents):
-        print(f"{n}) ({_size_hr(t['size'])})\'{t['name']}\'[{t['username']}], Seeds:{t['seeders']}, Leeches:{t['leechers']}: {_magnet_link(t['info_hash'])}")
+        print(f"{n}) ({_size_hr(t['size'])})\'{t['name']}\'[{t['username']}], Seeds:{t['seeders']}, Leeches:{t['leechers']}")
     try:
         selection = input("Enter the index of the torrent you want to use, or 'n' to skip: ")
         if selection == 'n':
             return "n"
+
         return _magnet_link(torrents[int(selection)]['info_hash'])
     except (ValueError, IndexError):
         print("Invalid index. Please enter a valid index.")
 
 
-def download_torrent(query):
+def download_torrent(query, download_flag=False):
     magnet = pick_torrent(query)
     if magnet and magnet != "n":
-        add_magnet_link(
-            config.get('URLS', 'qb_url'),
-            config.get('API_KEYS', 'qb_username'),
-            config.get('API_KEYS', 'qb_password'),
-            magnet
-        )
+        if download_flag:
+            add_magnet_link(
+                config.get('URLS', 'qb_url'),
+                config.get('API_KEYS', 'qb_username'),
+                config.get('API_KEYS', 'qb_password'),
+                magnet
+            )
+        else:
+            print(f"Selected: {magnet}")
+            print("Use the --download flag to queue this magnet link.")
 
 
 def _size_hr(size, decimals=2):
@@ -69,6 +74,7 @@ def _magnet_link(info_hash):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Search The Pirate Bay from the command line")
     parser.add_argument("query", help="Search query (enclose in quotes if it contains spaces)")
+    parser.add_argument("--download", action="store_true", help="Flag to add the queue torrent to your client")
     args = parser.parse_args()
 
-    download_torrent(args.query)
+    download_torrent(args.query, args.download)
