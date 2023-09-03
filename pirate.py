661781da064d2efcb92a242d8c73fba831af5bcf
davy <davy@windbornesystems.com>
2023-09-02 21:00:33 -0700

    Add option to skip.

--- pirate.py
+++ pirate.py
@@ -26,11 +26,11 @@ def pick_torrent(query):
 
     for n, t in enumerate(torrents):
         print(f"{n}) ({_size_hr(t['size'])})\'{t['name']}\'[{t['username']}], Seeds:{t['seeders']}, Leeches:{t['leechers']}: {_magnet_link(t['info_hash'])}")
-    if len(torrents) == 1:
-        return _magnet_link(torrents[0]['info_hash'])
     try:
-        i = int(input("Enter the index of the torrent you want to use: "))
-        return _magnet_link(torrents[i]['info_hash'])
+        selection = input("Enter the index of the torrent you want to use, or 'n' to skip: ")
+        if selection == 'n':
+            return
+        return _magnet_link(torrents[int(selection)]['info_hash'])
     except (ValueError, IndexError):
         print("Invalid index. Please enter a valid index.")
 
