4f76f60180cb8581dd5e54342eb40ef0cecdc297
davy <davy@windbornesystems.com>
2023-09-02 20:56:55 -0700

    Create util to add magnet link to qbtorrent.

---
+++ torrent.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def add_magnet_link(url, username, password, magnet):
+    auth_response = requests.post(
+        f"{url}/api/v2/auth/login",
+        data={"username": username, "password": password}
+    )
+    auth_cookie = auth_response.cookies.get("SID")
+
+    add_response = requests.post(
+        f"{url}/api/v2/torrents/add",
+        headers={"Cookie": f"SID={auth_cookie}"},
+        data={"urls": magnet}
+    )
+    if add_response.status_code != 200:
+        print("Error adding magnet link:", add_response.text)
