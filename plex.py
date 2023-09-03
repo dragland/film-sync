a0126168d99cfe7654d76b8329ce27948893e9d0
davy <davy@windbornesystems.com>
2023-09-02 20:16:58 -0700

    Create util for exporting current plex library.

---
+++ plex.py
@@ -0,0 +1,18 @@
+import requests
+import xml.etree.ElementTree as ET
+
+
+def get_library(url, token):
+    print("Pinging server...")
+    plex_url = f"{url}/library/sections/2/all"
+    response = requests.get(plex_url, headers={"X-Plex-Token": token})
+    xml = ET.fromstring(response.text)
+
+    library = set()
+    for f in xml.iter('Video'):
+        title = f.get('title')
+        year = f.get('year')
+        library.add((title, year))
+
+    print(f"You have {len(library)} films in your library")
+    return library
